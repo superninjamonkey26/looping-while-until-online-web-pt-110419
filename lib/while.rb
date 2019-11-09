b1c533bd5c9d55888f859900bb4dec679da028c6
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-09 22:36:39 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,12 @@
 def using_while
-  levitation_force = 6
-  while levitation_force < 10
+  using_while = 6
+  while using_while < 10
   puts "Wingardium Leviosa"
-  levitation_force += 1
+  using_while += 1
 end
 end
 
-puts levitation_force
+puts using_while
 
 
 
